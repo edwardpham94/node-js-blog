bada6ecb2124fea11400292c25c82ba4960cec97
edwardpham94 <phamhonggiabaoqn0904@gmail.com>
2023-05-27 16:07:49 +0700

    morgan

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')  // import
+const morgan = require('morgan')
 const app = express()  // object to build web
 const port = 3000
 
-app.get('/', (req, res) => { // duong dan 
+app.use(morgan('combined'))
+
+app.get('/tin-tuc', (req, res) => { // duong dan 
     res.send('Hello World!')
 })
 
